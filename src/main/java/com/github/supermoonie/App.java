887fcaddb4165b8c653e1b4944849e0260878792
supermoonie <wangchao_moonie@163.com>
2019-08-06 14:38:25 +0800

    update

--- src/main/java/com/github/supermoonie/App.java
+++ src/main/java/com/github/supermoonie/App.java
@@ -4,6 +4,7 @@ import com.github.supermoonie.auto.AutoChrome;
 import com.github.supermoonie.event.network.RequestWillBeSent;
 import com.github.supermoonie.handler.BytesResponseHandler;
 import com.github.supermoonie.handler.DocumentResponseHandler;
+import com.github.supermoonie.handler.StringResponseHandler;
 import com.github.supermoonie.httpclient.HttpClient;
 import com.github.supermoonie.todo.Todo;
 import com.github.supermoonie.type.network.GetResponseBodyResult;
@@ -24,6 +25,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
+import static com.github.supermoonie.util.StringUtils.isEmpty;
+
 /**
  * Hello world!
  *
@@ -37,33 +40,49 @@ public class App {
 
     private static final Pattern NUM_PATTERN = Pattern.compile("ts-(\\d+)\\.ts");
 
-    private static final Pattern LINK_PATTERN = Pattern.compile(".*(http.*)");
+    private static final Pattern LINK_PATTERN = Pattern.compile(".*(https?://[a-zA-Z0-9./]*)\".*");
 
     private static final Pattern VIDEO_NAME_PATTERN = Pattern.compile("([\\u4e00-\\u9fa5_a-zA-Z0-9]*).*");
 
     public static void main(String[] args) throws IOException {
         File videoDir = new File(VIDEO_PATH);
         if (!videoDir.exists() && !videoDir.mkdir()) {
-            JOptionPane.showMessageDialog(null,VIDEO_PATH + " 创建失败", "ERROR", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(null, VIDEO_PATH + " 创建失败", "ERROR", JOptionPane.ERROR_MESSAGE);
         }
         HttpClient httpClient = HttpClient.defaultInstance();
         String indexUrl = "http://goudaitv.com/vodplay/64845-1-43.html";
         Document document = httpClient.get(indexUrl, 3, DocumentResponseHandler.getInstance()).getContent();
-        Elements iframes = document.select("iframe");
-        if (null != iframes && iframes.size() > 0) {
-            for (Element iframe : iframes) {
-                String src = iframe.attr("src");
-                Matcher matcher = LINK_PATTERN.matcher(src);
-                if (matcher.find()) {
-                    indexUrl = matcher.group(1);
-                }
+        String frameSrc = findFrameSrc(document);
+        if (null != frameSrc) {
+            indexUrl = frameSrc;
+        }
+        try {
+            List<String> links = m3u8Content(httpClient, indexUrl);
+            int index = 100001;
+            for (String link : links) {
+                System.out.println("正在下载 " + link);
+                byte[] content = httpClient.get(link, 3, BytesResponseHandler.getInstance()).getContent();
+                FileUtils.writeByteArrayToFile(new File(VIDEO_PATH + "ts-" + index + ".ts"), content);
+                index++;
             }
+            concat();
+            rename(document.title());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static List<String> m3u8Content(HttpClient httpClient, String indexUrl) throws Exception {
+        if (indexUrl.endsWith(".m3u8")) {
+            String content = httpClient.get(indexUrl, 3, StringResponseHandler.getInstance()).getContent();
+            String baseUrl = indexUrl.substring(0, indexUrl.indexOf("/", 10));
+            String[] lines = content.split("\n");
+            return extractTsLinks(baseUrl, lines);
         }
         try (AutoChrome autoChrome = new AutoChrome.Builder()
 //                .setOtherArgs(Collections.singletonList("--headless"))
                 .build()) {
-            String iframeUrl = indexUrl;
-            Todo<NavigateResult> todo = (chrome) -> chrome.navigate(iframeUrl);
+            Todo<NavigateResult> todo = (chrome) -> chrome.navigate(indexUrl);
             RequestWillBeSentListener requestListener = new RequestWillBeSentListener("http*m3u8");
             autoChrome.waitEvent(todo, requestListener, 20_000);
             RequestWillBeSent request = requestListener.getRequestWillBeSent();
@@ -77,62 +96,77 @@ public class App {
             }
             String baseUrl = url.substring(0, url.indexOf("/", 10));
             String[] lines = responseContent.split("\n");
-            List<String> links = extractTsLinks(baseUrl, lines);
-            int index = 100001;
-            for (String link : links) {
-                System.out.println("正在下载 " + link);
-                byte[] content = httpClient.get(link, 3, BytesResponseHandler.getInstance()).getContent();
-                FileUtils.writeByteArrayToFile(new File(VIDEO_PATH + "ts-" + index + ".ts"), content);
-                index ++;
+            return extractTsLinks(baseUrl, lines);
+        }
+
+    }
+
+    private static String findFrameSrc(Document document) {
+        Elements iframes = document.select("iframe");
+        if (null != iframes && iframes.size() > 0) {
+            for (Element iframe : iframes) {
+                String src = iframe.attr("src");
+                if (isEmpty(src)) {
+                    src = iframe.outerHtml();
+                }
+                Matcher matcher = LINK_PATTERN.matcher(src);
+                if (matcher.find()) {
+                    return matcher.group(1);
+                }
             }
-            Collection<File> listFiles = FileUtils.listFiles(new File(VIDEO_PATH), new String[]{"ts"}, false);
-            List<File> fileList = listFiles.stream().sorted((o1, o2) -> {
-                String fileName1 = o1.getName();
-                String fileName2 = o2.getName();
-                Matcher matcher1 = NUM_PATTERN.matcher(fileName1);
-                if (matcher1.find()) {
-                    int num1 = Integer.parseInt(matcher1.group(1));
-                    Matcher matcher2 = NUM_PATTERN.matcher(fileName2);
-                    if (matcher2.find()) {
-                        int num2 = Integer.parseInt(matcher2.group(1));
-                        if (num2 > num1) {
-                            return -1;
-                        } else {
-                            return 1;
-                        }
+        }
+        return null;
+    }
+
+    private static void rename(String name) throws IOException {
+        File srcFile = new File(VIDEO_PATH + "111111111.mp4");
+        File destFile;
+        Matcher matcher = VIDEO_NAME_PATTERN.matcher(name);
+        if (matcher.find()) {
+            destFile = new File(VIDEO_PATH + matcher.group(1) + ".mp4");
+        } else {
+            destFile = new File(VIDEO_PATH + name.replaceAll("[^\\u4e00-\\u9fa5_a-zA-Z0-9]", "") + ".mp4");
+        }
+        FileUtils.moveFile(srcFile, destFile);
+    }
+
+    private static void concat() throws IOException {
+        Collection<File> listFiles = FileUtils.listFiles(new File(VIDEO_PATH), new String[]{"ts"}, false);
+        List<File> fileList = listFiles.stream().sorted((o1, o2) -> {
+            String fileName1 = o1.getName();
+            String fileName2 = o2.getName();
+            Matcher matcher1 = NUM_PATTERN.matcher(fileName1);
+            if (matcher1.find()) {
+                int num1 = Integer.parseInt(matcher1.group(1));
+                Matcher matcher2 = NUM_PATTERN.matcher(fileName2);
+                if (matcher2.find()) {
+                    int num2 = Integer.parseInt(matcher2.group(1));
+                    if (num2 > num1) {
+                        return -1;
+                    } else {
+                        return 1;
                     }
                 }
-                return 0;
-            }).collect(Collectors.toList());
-            List<String> paths = new ArrayList<>();
-            fileList.forEach(file -> paths.add("file '" + file.getAbsoluteFile() + "'"));
-            FileUtils.writeLines(new File(VIDEO_PATH + "ts.txt"), paths, false);
-            FFmpeg fFmpeg = new FFmpeg();
-            String srcFilePath = VIDEO_PATH + "111111111.mp4";
-            fFmpeg.run(new ArrayList<String>() {{
-                add("-f");
-                add("concat");
-                add("-safe");
-                add("0");
-                add("-i");
-                add(VIDEO_PATH + "ts.txt");
-                add("-c");
-                add("copy");
-                add(srcFilePath);
-            }});
-            fileList.forEach(file -> file.delete());
-            File srcFile = new File(srcFilePath);
-            File destFile;
-            Matcher matcher = VIDEO_NAME_PATTERN.matcher(document.title());
-            if (matcher.find()) {
-                destFile = new File(VIDEO_PATH + matcher.group(1) + ".mp4");
-            } else {
-                destFile = new File(VIDEO_PATH + document.title().replaceAll("[^\\u4e00-\\u9fa5_a-zA-Z0-9]", "") + ".mp4");
             }
-            FileUtils.moveFile(srcFile, destFile);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+            return 0;
+        }).collect(Collectors.toList());
+        List<String> paths = new ArrayList<>();
+        fileList.forEach(file -> paths.add("file '" + file.getAbsoluteFile() + "'"));
+        FileUtils.writeLines(new File(VIDEO_PATH + "ts.txt"), paths, false);
+        FFmpeg fFmpeg = new FFmpeg();
+        String srcFilePath = VIDEO_PATH + "111111111.mp4";
+        fFmpeg.run(new ArrayList<String>() {{
+            add("-f");
+            add("concat");
+            add("-safe");
+            add("0");
+            add("-i");
+            add(VIDEO_PATH + "ts.txt");
+            add("-c");
+            add("copy");
+            add(srcFilePath);
+        }});
+        fileList.forEach(File::delete);
     }
 
     private static List<String> extractTsLinks(String baseUrl, String[] lines) {
--- src/test/java/com/github/supermoonie/AppTest.java
+++ src/test/java/com/github/supermoonie/AppTest.java
@@ -3,6 +3,8 @@ package com.github.supermoonie;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static org.junit.Assert.assertTrue;
 
@@ -10,11 +12,17 @@ import static org.junit.Assert.assertTrue;
  * Unit test for simple App.
  */
 public class AppTest {
+
     /**
      * Rigorous Test :-)
      */
     @Test
     public void shouldAnswerWithTrue() throws IOException {
+        String text = "<iframe id=\"fed-play-iframe\" class=\"fed-play-iframe fed-part-full\" frameborder=\"0\" scrolling=\"no\" allowfullscreen=\"true\" data-lock=\"0\" data-pass=\"0\" data-auto=\"1\" data-seek=\"0\" data-code=\"1\" data-trys=\"0\" data-advs=\"0\" data-link=\"\" data-time=\"5\" data-chat=\"0\" data-word=\"SZk\" data-stat=\"1\" data-pars=\"/template/vfed/asset/fed/player.php?id=peer&amp;url=\" data-play=\"https://cn3.download05.com/hls/20190805/97e745627c1b982a1dbf0cd34bff224b/1565009347/index.m3u8\" data-next=\"\"></iframe>";
+        Matcher matcher = Pattern.compile(".*(https?://[a-zA-Z0-9./]*)\".*").matcher(text);
+        if (matcher.find()) {
+            System.out.println(matcher.group(1));
+        }
         assertTrue(true);
     }
 }
